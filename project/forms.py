1abfd51793de52e3cf5dd84ee65916eea8961ef0
akib ali <akib2900ali@gmail.com>
2023-01-29 17:37:55 +0530

    first commit

---
+++ project/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+
+
+
+class UsersForm(forms.Form):
+    num1=forms.CharField()
+    num2=forms.CharField()
--- project/views.py
+++ project/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+from  .forms import UserForm
 
 def aboutUs(request):
     return render (request,'index.html')
 
 def userform(request):
     finalans=0
+    fn=UserForm()
+    data={'form':fn}
     try:
      if request.method=="post":  
       n1=int(request.post['num1'])
